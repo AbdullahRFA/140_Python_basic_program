e50ce1a850ddcdc9cb0cf33d33ba7ecdd5357624
AbdullahRFA <shakibrybmn@gmail.com>
2025-02-03 21:21:18 +0600

    Write a Python program to convert Celsius to Fahrenheit.

---
+++ 7.Write a Python program to convert Celsius to Fahrenheit.py
@@ -0,0 +1,6 @@
+# Enter temperature in Celsius: 37
+# 37.0 degrees Celsius is equal to 98.6 degrees Fahrenheit
+celsius = float(input("Enter temperature in Celsius: "))
+# Conversion formula: Fahrenheit = (Celsius * 9/5) + 32
+fahrenheit = (celsius * 9/5) + 32
+print(f"{celsius} degrees Celsius is equal to {fahrenheit} degrees Fahrenheit")
