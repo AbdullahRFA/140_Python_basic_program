142e63880e07a6d96a0ebebc1c7e089616f7125e
AbdullahRFA <shakibrybmn@gmail.com>
2025-03-20 14:33:27 +0600

    Write a program which takes 2 digits, X,Y as input and generates a 2-dimensional array. The element value in the i-th row and j-th column of the array should be i*j.

--- test.py
+++ test.py
@@ -1,6 +1,18 @@
-a, b, c = 4
+import math
+def is_prime(x):
+    for i in range(2, int(math.sqrt(x)+1)):
+        if x % i == 0:
+            return False
+    return True
+my_set = set()
+def print_prime_factors(n):
+    for i in range(2, n + 1):
+        if is_prime(i):
+            while n % i == 0:
+                print(i)
+                my_set.add(i)
+                n/=i
 
-if a+b == 8:
-    print("GFG")
-else:
-    print("Hello")
+n = 100
+print(my_set)
+print_prime_factors(n)
